e82b1d4a3f90c757d7041fe655bb7fdf4fb8af05
Marco Cerilli <cerillimarco15@gmail.com>
2026-08-17 15:04:22 +0200

    feat: add interactive Company Profile PDF generator and update robots.txt exclusion rules

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
         disallow: [
           '/api/',        // Protegge le rotte dei dati
-          '/_next/',       // Nasconde i file di build interni
           '/admin/',       // Protegge eventuali aree riservate
           '/private/',     // Cartelle di test o bozze
         ],
